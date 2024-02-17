e957a0216d8f1bf88b4075a7aa81e8d9f5089ccb
Miekale Smith <miekale.smith@gmail.com>
2024-02-17 18:15:01 -0500

    untested data synthesis without overlap detection

---
+++ data_synthesis.py
@@ -0,0 +1,72 @@
+import math
+
+class Synthesis:
+    def __init__(self, left_obj, right_obj, filter = None):
+        #objects in the form of [[x, y, w, h, class name]] x, y (0,0) being top left
+        self.width = 640
+        self.height = 480
+        self.radius = 30
+        self.intersection = .9
+        self.ANGLE = 110
+
+        if filter != None:
+            for count in range(len(left_obj) - 1, 0, -1):
+                if left_obj[count][4] in filter:
+                    left_obj.pop(count)
+
+            for count in range(len(right_obj) - 1, 0, -1):
+                if right_obj[count][4] in filter:
+                    right_obj.pop(count)
+        
+        self.left = left_obj
+        self.right = right_obj
+
+    def removeIntersection(self):
+        print("hello")
+
+    def getSpacial(self, x, y, w, h, isRight):
+        x = x + w // 2
+        y = y - h // 2
+        if not isRight:
+            x = x - self.width
+        
+        angle = x / self.width * self.ANGLE
+        
+        x = int(self.radius * math.sin(angle))
+        y = int(self.radius * math.cos(angle))
+
+        return angle, [x, y, 0]
+    
+    def getSentence(self, angle, isRight, className):
+        #removing negative angle
+        angle = angle if isRight else -1 * angle 
+        direction = "right" if isRight else "left"
+        
+        return "A " + className + " is " + angle + " to the " + direction 
+
+
+    def transform(self, isRight : bool, view):
+        #output [[string, 3d pos]]
+        output = []
+        direction = "right" if isRight else "left"
+        for object in view:
+            angle, quards = self.getSpacial(object[0], object[1], object[2], object[3], isRight)
+
+            sentence = self.getSentence(self, angle, isRight, object[4])
+
+            output.append(sentence, quards)
+        return output
+
+    def setRadius(self, radius):
+        self.radius = radius
+
+    def output(self):
+        #self.removeIntersection()
+        left = self.transform(isRight=False, view = self.left)
+        right = self.transform(isRight = True, view = self.right)
+        return left + right
+            
+        
+            
+
+    
---
+++ main.py
@@ -0,0 +1,8 @@
+
+class BlindWatchers:
+    def __init__(self):
+
+    def run(self):
+
+
+if __name__ == "__main__":
