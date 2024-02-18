073393569b4ba0aaf79749059d49d33fc2d984a9
hepromark <markdo28102004@gmail.com>
2024-02-17 19:41:27 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,19 @@
+from data_synthesis import Synthesis
+from camera_driver import CameraDriver
 
 class BlindWatchers:
     def __init__(self):
+        pass
 
     def run(self):
+        pass
 
 
 if __name__ == "__main__":
+    cam = CameraDriver(0, 1)
+    left_detection, right_detection = cam.detect()
+
+    syn = Synthesis(left_detection, right_detection, ["person"])
+    output = syn.output()
+    print(len(output))
+    print(output)
